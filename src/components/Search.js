a298a5610c8f9442b35877b9545c3c50da818958
madisonmincevich <maddiebooldridge@gmail.com>
2022-10-31 12:06:08 -0800

    updated search

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,10 +6,14 @@ import "../styling/Search.css";
 export default function Search(props) {
   let [searchResults, setSearchResults] = useState(null);
   let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [loaded, setLoaded] = useState(false);
+  function load() {
+    setLoaded(true);
+    searched();
+  }
 
   function handleTheResponse(response) {
     setSearchResults(response.data);
-    console.log(response.data);
   }
 
   function handleSearchClick(e) {
@@ -23,22 +27,36 @@ export default function Search(props) {
 
   function searched() {
     let apiLink = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword}`;
-    axios.get(apiLink).then(handleTheResponse);
+    axios
+      .get(apiLink)
+      .then(handleTheResponse)
+      .catch(function (error) {
+        if (error) {
+          console.log("No cocktail");
+        } else {
+          console.log("it works");
+        }
+      });
   }
 
-  return (
-    <div className="m-5 pt-5">
-      <h1 className="searchTitle">Search for Your Drink</h1>
-      <form onSubmit={handleSearchClick}>
-        <input
-          type="text"
-          defaultValue={props.defaultKeyword}
-          onChange={input}
-          className="m-2"
-        />
-        <input type="submit" placeholder="Search" className="searchBtn" />
-      </form>
-      <SearchResults searchResults={searchResults} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="m-5 pt-5">
+        <h1 className="searchTitle">Search for Your Drink</h1>
+        <form onSubmit={handleSearchClick}>
+          <input
+            type="text"
+            defaultValue={props.defaultKeyword}
+            onChange={input}
+            className="m-2"
+          />
+          <input type="submit" placeholder="Search" className="searchBtn" />
+        </form>
+        <SearchResults searchResults={searchResults} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
